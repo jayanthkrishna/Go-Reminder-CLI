8ce164cf43c27b0eeb9f5a61fb18a34f95c32ba8
jayanthkrishna <gannerla16038@iiitd.ac.in>
2022-10-23 23:55:17 +0530

    Notifier logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"github.com/whyrusleeping/go-notifier"
+)
+
 func main() {
+
+	var n notifier.Notifier
+
+	n.Notify(map[string]interface{}{
+		"title":      "Unknown title",
+		"message":    "Unknown message",
+		"sound":      true,
+		"wait":       true,
+		"reply":      true,
+		"closeLabel": "Completed?",
+		"timeout":    15,
+	})
 
 }
